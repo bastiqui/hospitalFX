1df12c1b31d1079b65df6f070dd1367f717c11ef
vanazco <vanazco@elpuig.xeill.net>
2019-01-31 20:00:04 +0100

    cambios Menu.fxml

--- src/main/java/WindowController.java
+++ src/main/java/WindowController.java
@@ -1,13 +1,3 @@
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-
-import java.awt.event.ActionEvent;
-
 public class WindowController {
-    public Button btnSearch, btnList, BtnFile;
 
-    @FXML
-    public void VerLista (ActionEvent event) {
-
-    }
 }
