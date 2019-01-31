97b7ee3d45f1ba1851f81645868088fd10bf1599
Andrés Quiceno <asquicenoh97@elpuig.xeill.net>
2019-01-31 19:18:44 +0100

    All new files added

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import com.opencsv.CSVReader;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        AnchorPane pane = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("window.fxml")));
+        stage.setScene(new Scene(pane));
+        stage.setTitle("prova jxf maven");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
