cb53aec5ec31629176d41b6fccbbc3c2890b7a89
Andrés Quiceno <asquicenoh97@elpuig.xeill.net>
2019-01-31 19:58:58 +0100

    Added function to view list

--- src/main/java/WindowController.java
+++ src/main/java/WindowController.java
@@ -1,2 +1,13 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
 public class WindowController {
+    public Button btnSearch, btnList, BtnFile;
+
+    @FXML
+    public void VerLista (ActionEvent event) {
+
+    }
 }
