a2cf29b72b2d020ea1fadb543d9e0b904d9ce93b
Andrés Quiceno <asquicenoh97@elpuig.xeill.net>
2019-01-31 20:10:28 +0100

    VerLista added and working

--- src/main/java/WindowController.java
+++ src/main/java/WindowController.java
@@ -4,11 +4,8 @@ import javafx.scene.control.Button;
 import java.awt.event.ActionEvent;
 
 public class WindowController {
-
     public Button btnSearch, btnList, BtnFile;
 
-    @FXML
-    public void VerLista (ActionEvent event) {
-
+    public void VerLista(javafx.event.ActionEvent event) {
     }
 }
