37707a0643eff80e9f923bca415ea5f0f571b494
Andrés Quiceno <asquicenoh97@elpuig.xeill.net>
2019-02-05 17:36:25 +0100

    MAVEN now has JFOENIX added

--- src/main/java/WindowController.java
+++ src/main/java/WindowController.java
@@ -3,6 +3,7 @@ import Model.Pacient;
 import Model.Persona;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -22,55 +23,71 @@ public class WindowController {
 
     private List<Pacient> pacientList = new ArrayList<>();
     private boolean listaCargada = false;
+    private boolean tablaHecha = false;
+    private Hospital hospital = new Hospital();
+    private File csvPath = new File("");
 
-    public void VerLista(javafx.event.ActionEvent event) {
+    private void alerta(String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Ves amb compte!");
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    private void CamposTabla() {
+        TableColumn DNI = new TableColumn("DNI");
+        TableColumn Nom = new TableColumn("Nom");
+        TableColumn Cognoms = new TableColumn("Cognoms");
+        TableColumn DataNaix = new TableColumn("Data de Naixament");
+        TableColumn Genre = new TableColumn("Gènere");
+        TableColumn pes = new TableColumn("Pes");
+        TableColumn Alcada = new TableColumn("Alçada");
+        TableColumn Telefon = new TableColumn("Telèfon");
+
+        // COMPTE!!!! les propietats han de tenir getters i setters
+        DNI.setCellValueFactory(new PropertyValueFactory<Pacient, String>("DNI"));
+        Nom.setCellValueFactory(new PropertyValueFactory<Pacient, String>("Nom"));
+        Cognoms.setCellValueFactory(new PropertyValueFactory<Pacient, String>("Cognoms"));
+        DataNaix.setCellValueFactory(new PropertyValueFactory<Pacient, String>("DataNaixament"));
+        Genre.setCellValueFactory(new PropertyValueFactory<Pacient, String>("genere"));
+        pes.setCellValueFactory(new PropertyValueFactory<Pacient, Float>("Pes"));
+        Alcada.setCellValueFactory(new PropertyValueFactory<Pacient, Integer>("Alçada"));
+        Telefon.setCellValueFactory(new PropertyValueFactory<Pacient, String>("Telefon"));
+
+        tblView.getColumns().addAll(DNI, Nom, Cognoms, DataNaix, Genre, pes, Alcada, Telefon);
+        tablaHecha = true;
+    }
+
+    public void VerLista() {
         if (listaCargada) {
-            Node node = (Node) event.getSource();
+            tblView.refresh();
+            pacientList.clear();
 
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Escull el CSV de pacients.");
-            File file = fileChooser.showOpenDialog(node.getScene().getWindow());
-
-            TableColumn DNI = new TableColumn("DNI");
-            TableColumn Nom = new TableColumn("Nom");
-            TableColumn Cognoms = new TableColumn("Cognoms");
-            TableColumn Email = new TableColumn("Email");
-            TableColumn DataNaix = new TableColumn("Data de Naixament");
-            TableColumn Genre = new TableColumn("Gènere");
-            TableColumn pes = new TableColumn("Pes");
-            TableColumn Alcada = new TableColumn("Alçada");
-            TableColumn Telefon = new TableColumn("Telèfon");
-
-            // COMPTE!!!! les propietats han de tenir getters i setters
-            DNI.setCellValueFactory(new PropertyValueFactory<Pacient, String>("DNI"));
-            Nom.setCellValueFactory(new PropertyValueFactory<Pacient, String>("Nom"));
-            Cognoms.setCellValueFactory(new PropertyValueFactory<Pacient, String>("Cognoms"));
-            DataNaix.setCellValueFactory(new PropertyValueFactory<Pacient, String>("DataNaixament"));
-            Genre.setCellValueFactory(new PropertyValueFactory<Pacient, String>("genere"));
-            pes.setCellValueFactory(new PropertyValueFactory<Pacient, Float>("Pes"));
-            Alcada.setCellValueFactory(new PropertyValueFactory<Pacient, Integer>("Alçada"));
-            Telefon.setCellValueFactory(new PropertyValueFactory<Pacient, String>("Telefon"));
-
-            tblView.getColumns().addAll(DNI, Nom, Cognoms, DataNaix, Genre, pes, Alcada, Telefon);
+            if (!tablaHecha) CamposTabla();
 
             ObservableList<Pacient> data = FXCollections.observableArrayList();
-            data.add(new Pacient("12345678C", "Test", "Java", LocalDate.of(2019, 12, 12), Persona.Genere.DONA, "555555599", 5.4f, 100));
-            loadData(file);
+            data.add(new Pacient("12345678C", "Test", "Java", LocalDate.of(2019, 12, 12), Persona.Genere.DONA, "55555599", 5.4f, 100));
+            loadData();
             data.addAll(pacientList);
             tblView.setItems(data);
         } else {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Ves amb compte!");
-            alert.setHeaderText("No n'hi han dades.");
-            alert.setContentText("No s'ha carregat cap dada al programa.");
-            alert.showAndWait();
+            alerta("No hi han dades.", "No s'ha carregat cap dada al programa.");
         }
     }
 
-    private void loadData(File csvPath) {
-        Hospital hospital = new Hospital();
+    private void loadData() {
         pacientList.addAll(hospital.loadPacients(csvPath));
     }
 
+    public void ChooseFile(ActionEvent event) {
+        Node node = (Node) event.getSource();
 
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Escull el CSV de pacients.");
+        csvPath = fileChooser.showOpenDialog(node.getScene().getWindow());
+        listaCargada = hospital.checkCSV(csvPath);
+        if (!listaCargada) alerta("Fitxer not vàlid.", "El programa no pot llegir aquest fitxer");
+        else VerLista();
+    }
 }
