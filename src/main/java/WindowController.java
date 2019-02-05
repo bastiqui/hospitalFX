bec5ee0d5d9c5ca62a7c3a85c7711af00d17fc51
Andrés Quiceno <asquicenoh97@elpuig.xeill.net>
2019-02-05 16:41:50 +0100

    Controller now opens a windows to select the file

--- src/main/java/WindowController.java
+++ src/main/java/WindowController.java
@@ -1,13 +1,18 @@
 import Model.Hospital;
 import Model.Pacient;
 import Model.Persona;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,10 +22,15 @@ public class WindowController {
 
     public TableView<Pacient> tblView;
 
-    String csvPath = "src/main/java/CSV/pacients.csv";
     private List<Pacient> pacientList = new ArrayList<>();
 
     public void VerLista(javafx.event.ActionEvent event) {
+        Node node = (Node) event.getSource();
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Escull el CSV de pacients.");
+        File file = fileChooser.showOpenDialog(node.getScene().getWindow());
+
         TableColumn DNI = new TableColumn("DNI");
         TableColumn Nom = new TableColumn("Nom");
         TableColumn Cognoms = new TableColumn("Cognoms");
@@ -45,12 +55,12 @@ public class WindowController {
 
         ObservableList<Pacient> data = FXCollections.observableArrayList();
         data.add(new Pacient("12345678C", "Test", "Java", LocalDate.of(2019,12,12), Persona.Genere.DONA, "555555599", 5.4f, 100));
-        loadData();
+        loadData(file);
         data.addAll(pacientList);
         tblView.setItems(data);
     }
 
-    private void loadData() {
+    private void loadData(File csvPath) {
         Hospital hospital = new Hospital();
         pacientList.addAll(hospital.loadPacients(csvPath));
     }
