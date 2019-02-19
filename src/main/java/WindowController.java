a7c11f09132e060f0d792e825a98784f59117bfc
Andrés Quiceno <asquicenoh97@elpuig.xeill.net>
2019-02-19 16:21:32 +0100

    Added textfields

--- src/main/java/WindowController.java
+++ src/main/java/WindowController.java
@@ -5,10 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
 
@@ -20,6 +17,8 @@ import java.util.List;
 public class WindowController {
     public Button btnSearch, btnList, BtnFile;
     public TableView<Pacient> tblView;
+    public TextField WeightF, WeightT, AgeF, AgeT, HeightF, HeightT;
+    public TextField SearchDni, SearchSurname, SearchName;;
 
     private List<Pacient> pacientList = new ArrayList<>();
     private boolean listaCargada = false;
